5fca9cda149371dddf607a097c5f479543587eb2
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-09-15 16:23:22 +0500

    first commit

---
+++ ex1.js
@@ -0,0 +1,4 @@
+// Exercise-1: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+var myName = 'ali'
+console.log(`Hello ${myName}, would you like to learn some Python today?`)
---
+++ ex2.js
@@ -0,0 +1,8 @@
+// Exercise-1:Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var myName = 'aliraza'
+console.log(`UpperCase :${myName.toUpperCase()}`)
+console.log(`LowerCase :${myName.toLowerCase()}`)
+const newName = myName.split('')
+for (let index = 0; index < newName.length; index++) {
+  const element = newName[index]
+}
