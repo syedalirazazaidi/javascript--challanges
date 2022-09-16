01af11879e59f4c074c9ecf6391b1f9e8150f7ad
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-09-16 23:49:27 +0500

    exercise 3

---
+++ ex3.js
@@ -0,0 +1,10 @@
+// Exercise-3:Famous Quote: Find a quote from a famous person you admire.
+// Print the quote and the name of its author.
+// Your output should look something like the following,
+// including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+var famousPersion = 'Albert Einstein'
+var quote = '“A person who never made a mistake never tried anything new.”'
+
+console.log(`${famousPersion}  once said,${quote}`)
