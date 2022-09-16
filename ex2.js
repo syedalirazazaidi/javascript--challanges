24ae6b80c2dd2977b0ee93ab41f620ea90c6294a
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-09-16 12:42:53 +0500

    two

--- ex2.js
+++ ex2.js
@@ -1,8 +1,19 @@
 // Exercise-1:Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-var myName = 'aliraza'
+var myName = 'aliraza zaidi'
 console.log(`UpperCase :${myName.toUpperCase()}`)
 console.log(`LowerCase :${myName.toLowerCase()}`)
-const newName = myName.split('')
-for (let index = 0; index < newName.length; index++) {
-  const element = newName[index]
+var newName = myName.split(' ')
+console.log(newName, 'newName')
+for (var index = 0; index < newName.length; index++) {
+  var element = newName[index].charAt(0).toUpperCase() + newName[index].slice(1)
+  console.log(element)
 }
+
+function toTitleCase(str) {
+  str = str.toLowerCase().split(' ')
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1)
+  }
+  return str.join(' ')
+}
+console.log(toTitleCase('hi this is a software engineer'))
