ab000fa58a9a1dbb1fdf3df75787e0b23a702876
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-09-30 22:59:10 +0500

    Add problem 17

--- ex17.js
+++ ex17.js
@@ -1,28 +1,35 @@
 // problem:17=Shrinking Guest List: You just found out that your
-//  new dinner table won’t arrive in time for the dinner, 
+// new dinner table won’t arrive in time for the dinner,
 // and you have space for only two guests.
 // • Start with your program from Exercise 16. Add a new line
 // that prints a message saying that you can invite only two people for dinner.
 
-// • Remove guests from your list one at a time until only two 
-// names remain in your list. Each time you pop a name from your 
-// list, print a message to that person letting them know you’re sorry you can’t 
+// • Remove guests from your list one at a time until only two
+// names remain in your list. Each time you pop a name from your
+// list, print a message to that person letting them know you’re sorry you can’t
 // invite them to dinner.
 
 // • Print a message to each of the two people still on your list,
 //  letting them know they’re still invited.
 
-// • Remove the last two names from your list, so you have an empty list. 
+// • Remove the last two names from your list, so you have an empty list.
 // Print your list to make sure you actually have an empty list at the end of your program
 
-const newArrays = ['ali', 'daniyal', 'khurram']
-newArrays.unshift('syed', 'ahmed', 'mehmood')
+const newArrays = ['ali', 'daniyal', 'khurram', 'mehmood']
 
-newArrays.splice(newArrays.length / 2, 0, 'Lene')
-newArrays.push('hamzah')
-console.log(newArrays)
-var element = []
-for (let index = 0; index < newArrays.length; index++) {
-  const newElem = newArrays[index]
-  console.log(newElem, 'you are invited for a bigger dinner table')
+var newElem = newArrays[0]
+var newValue = newArrays[1]
+
+console.log('you are invite only two people for dinner', newElem, '&', newValue)
+for (var index = 0; index < newArrays.length; index++) {
+  if (newArrays.length >= 2) {
+    var element = newArrays[index]
+
+    console.log('sorry', element, 'we  can’t invite them to dinner.')
+    newArrays.shift()
+  }
+  if (newArrays.length <= 2) {
+    // var element = newArrays[index]
+    // console.log('you are invited for a dinner', element)
+  }
 }
