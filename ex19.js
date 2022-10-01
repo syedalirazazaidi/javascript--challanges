f0dfd995331741464271da6a66ae60ea416618d0
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-01 09:59:47 +0500

    add 19

---
+++ ex19.js
@@ -0,0 +1,4 @@
+// problem:19=Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people
+// you are inviting to dinner.
+
+console.log('8 number of people we are inviting for a  dinner.')
