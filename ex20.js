12cca841e0b436517bc40ffda1347d7030d4b382
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-01 10:07:34 +0500

    Add ex20

---
+++ ex20.js
@@ -0,0 +1,9 @@
+// problem:20=Think of something you could store in a array.
+//  For example, you could make a list of mountains, rivers,
+//   countries, cities, languages, or anything else you’d like. Write a program that
+//    creates a list containing these items.
+const mountainsArray = []
+mountainsArray.push('K-2')
+mountainsArray.push('Mountain')
+mountainsArray.push('Everst')
+console.log(mountainsArray)
